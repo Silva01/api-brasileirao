736d83c15cac15f2fbfa70bf6cba89f1942f25d7
silva01 <silva20102@gmail.com>
2023-04-29 16:54:34 -0300

    Initial Commit

---
+++ src/main/java/br/net/silva/daniel/apibrasileirao/ApiBrasileiraoApplication.java
@@ -0,0 +1,13 @@
+package br.net.silva.daniel.apibrasileirao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBrasileiraoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBrasileiraoApplication.class, args);
+    }
+
+}
