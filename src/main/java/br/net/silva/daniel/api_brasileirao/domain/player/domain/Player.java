8c4b3a4791f0a8ca3a6f031a24200fd819090d24
silva01 <silva20102@gmail.com>
2023-04-29 20:33:20 -0300

    Modified class for representation player

--- src/main/java/br/net/silva/daniel/api_brasileirao/domain/player/domain/Player.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/domain/player/domain/Player.java
@@ -1,13 +1,20 @@
 package br.net.silva.daniel.api_brasileirao.domain.player.domain;
 
+import br.net.silva.daniel.api_brasileirao.domain.team.domain.Team;
+
+import java.time.LocalDate;
+
 public class Player {
 
     private final String name;
-    private final String localidade;
+    private final LocalDate birthDate;
+    private final String country;
+    private final Team team;
 
-    public Player(final String name, final String localidade) {
+    public Player(String name, LocalDate birthDate, String country, Team team) {
         this.name = name;
-        this.localidade = localidade;
+        this.birthDate = birthDate;
+        this.country = country;
+        this.team = team;
     }
-
 }
