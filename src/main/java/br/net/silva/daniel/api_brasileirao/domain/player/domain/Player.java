0b4dc3bdfbe07cc52d5c6aba5b6aeb45e595e1ad
silva01 <silva20102@gmail.com>
2023-04-29 20:30:38 -0300

    Created class for representation player

---
+++ src/main/java/br/net/silva/daniel/api_brasileirao/domain/player/domain/Player.java
@@ -0,0 +1,13 @@
+package br.net.silva.daniel.api_brasileirao.domain.player.domain;
+
+public class Player {
+
+    private final String name;
+    private final String localidade;
+
+    public Player(final String name, final String localidade) {
+        this.name = name;
+        this.localidade = localidade;
+    }
+
+}
