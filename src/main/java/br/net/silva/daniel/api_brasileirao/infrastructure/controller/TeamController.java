ad630b65cd65722e040b1e8602942100f24e0c1b
silva01 <silva20102@gmail.com>
2023-05-06 18:06:52 -0300

    Implemented endpoint for create team

--- src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/controller/TeamController.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/controller/TeamController.java
@@ -1,6 +1,10 @@
 package br.net.silva.daniel.api_brasileirao.infrastructure.controller;
 
+import br.net.silva.daniel.api_brasileirao.domain.shared.repository.SaveRepository;
+import br.net.silva.daniel.api_brasileirao.domain.team.domain.Team;
 import br.net.silva.daniel.api_brasileirao.domain.team.dto.TeamDTO;
+import br.net.silva.daniel.api_brasileirao.usecase.team.domain.SaveTeamUseCase;
+import br.net.silva.daniel.api_brasileirao.usecase.team.interfaces.UseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +17,17 @@ import java.util.concurrent.Callable;
 @RequestMapping("/team")
 public class TeamController {
 
+    private final SaveRepository<Team> saveRepository;
+
+    public TeamController(SaveRepository<Team> saveRepository) {
+        this.saveRepository = saveRepository;
+    }
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public void create() {
-        // TODO: Doing
+    public void create(@RequestBody final TeamDTO teamDTO) {
+        UseCase<Team> createUseCase = new SaveTeamUseCase(saveRepository, new Team(teamDTO.getName(), teamDTO.getLocalidade()));
+        createUseCase.execute();
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
