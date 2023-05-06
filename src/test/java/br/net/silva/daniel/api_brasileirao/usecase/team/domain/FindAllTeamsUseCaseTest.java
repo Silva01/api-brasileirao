d5cea327e1f979716208091174df4b466da92c6f
silva01 <silva20102@gmail.com>
2023-05-06 18:50:08 -0300

    Created use case unit tests for find all teams

---
+++ src/test/java/br/net/silva/daniel/api_brasileirao/usecase/team/domain/FindAllTeamsUseCaseTest.java
@@ -0,0 +1,40 @@
+package br.net.silva.daniel.api_brasileirao.usecase.team.domain;
+
+import br.net.silva.daniel.api_brasileirao.domain.shared.repository.FindAllRepository;
+import br.net.silva.daniel.api_brasileirao.domain.shared.repository.SaveRepository;
+import br.net.silva.daniel.api_brasileirao.domain.team.domain.Team;
+import br.net.silva.daniel.api_brasileirao.infrastructure.ApiBrasileiraoApplication;
+import br.net.silva.daniel.api_brasileirao.usecase.team.interfaces.UseCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = ApiBrasileiraoApplication.class)
+class FindAllTeamsUseCaseTest {
+
+    private final FindAllRepository<Team> findAllRepository;
+    private final SaveRepository<Team> saveRepository;
+
+    @Autowired
+    FindAllTeamsUseCaseTest(FindAllRepository<Team> findAllRepository, SaveRepository<Team> saveRepository) {
+        this.findAllRepository = findAllRepository;
+        this.saveRepository = saveRepository;
+    }
+
+    @Test
+    public void deve_retornar_todos_os_times() {
+        UseCase<List<Team>> findAllTeamsUseCase = new FindAllTeamsUseCase(findAllRepository);
+        UseCase<Team> saveTeamUseCase = new SaveTeamUseCase(saveRepository, new Team("Flamengo", "RJ"));
+
+        saveTeamUseCase.execute();
+        List<Team> response = findAllTeamsUseCase.execute();
+
+        assertEquals(response.size(), 1);
+        assertEquals(response.get(0).getAggregate().getName(), "Flamengo");
+        assertEquals(response.get(0).getAggregate().getLocalidade(), "RJ");
+    }
+}
