888fdccb75a05dd51fb91a6fcadea2a3d368b524
silva01 <silva20102@gmail.com>
2023-05-06 18:07:12 -0300

    created teamDTO

--- src/main/java/br/net/silva/daniel/api_brasileirao/domain/team/dto/TeamDTO.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/domain/team/dto/TeamDTO.java
@@ -1,4 +1,23 @@
 package br.net.silva.daniel.api_brasileirao.domain.team.dto;
 
 public class TeamDTO {
+
+    private String name;
+    private String localidade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
 }
