356fb0040e555824e523fefd00900847a5eab99c
silva01 <silva20102@gmail.com>
2023-05-06 18:08:09 -0300

    mapped components for principal class load of spring

--- src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/ApiBrasileiraoApplication.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/ApiBrasileiraoApplication.java
@@ -2,8 +2,10 @@ package br.net.silva.daniel.api_brasileirao.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"br.net.silva.daniel.api_brasileirao", "br.net.silva.daniel.api_brasileirao.*"})
 public class ApiBrasileiraoApplication {
 
     public static void main(String[] args) {
