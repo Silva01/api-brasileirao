83bf4cb3c7e1f94b20d1d3785d84271113b7f044
silva01 <silva20102@gmail.com>
2023-05-06 16:25:13 -0300

    Created model for Aggregate

--- src/main/java/br/net/silva/daniel/api_brasileirao/domain/team/domain/Team.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/domain/team/domain/Team.java
@@ -1,11 +1,25 @@
 package br.net.silva.daniel.api_brasileirao.domain.team.domain;
 
-public class Team {
+import br.net.silva.daniel.api_brasileirao.domain.shared.interfaces.Aggregate;
+
+public class Team implements Aggregate<Team> {
     private final String name;
     private final String localidade;
 
     public Team(final String name, final String localidade) {
         this.name = name;
         this.localidade = localidade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+    @Override
+    public Team getAggregate() {
+        return this;
     }
 }
