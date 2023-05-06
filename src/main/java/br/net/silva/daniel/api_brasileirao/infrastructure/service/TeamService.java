b0bdeb54984f6873378ec4d86a3eabea82b98ba6
silva01 <silva20102@gmail.com>
2023-05-06 18:51:24 -0300

    Implemented find all method

--- src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/service/TeamService.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/service/TeamService.java
@@ -1,5 +1,6 @@
 package br.net.silva.daniel.api_brasileirao.infrastructure.service;
 
+import br.net.silva.daniel.api_brasileirao.domain.shared.repository.FindAllRepository;
 import br.net.silva.daniel.api_brasileirao.domain.shared.repository.SaveRepository;
 import br.net.silva.daniel.api_brasileirao.domain.team.domain.Team;
 import br.net.silva.daniel.api_brasileirao.infrastructure.model.TeamModel;
@@ -7,8 +8,10 @@ import br.net.silva.daniel.api_brasileirao.infrastructure.repository.TeamReposit
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class TeamService implements SaveRepository<Team> {
+public class TeamService implements SaveRepository<Team>, FindAllRepository<Team> {
 
     private TeamRepository repository;
 
@@ -24,5 +27,10 @@ public class TeamService implements SaveRepository<Team> {
         model.setLocalidade(aggregate.getLocalidade());
         repository.save(model);
         return aggregate;
+    }
+
+    @Override
+    public List<Team> findAll() {
+        return repository.findAll().stream().map(teamModel -> new Team(teamModel.getName(), teamModel.getLocalidade())).toList();
     }
 }
