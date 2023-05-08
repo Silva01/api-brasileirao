ee44057b8b792f6aed7bc5a207a1ca31df396ce9
silva01 <silva20102@gmail.com>
2023-05-08 13:52:26 -0300

    Removed imports not use

--- src/main/java/br/net/silva/daniel/api_brasileirao/usecase/team/interfaces/UseCase.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/usecase/team/interfaces/UseCase.java
@@ -1,6 +1,5 @@
 package br.net.silva.daniel.api_brasileirao.usecase.team.interfaces;
 
-import br.net.silva.daniel.api_brasileirao.domain.shared.interfaces.Aggregate;
 
 public interface UseCase <T> {
     T execute();
