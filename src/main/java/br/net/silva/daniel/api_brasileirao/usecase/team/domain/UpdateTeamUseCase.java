a640f9d171056997e5f5cd2309009c9228730373
silva01 <silva20102@gmail.com>
2023-05-08 14:31:16 -0300

    Created Update team use case

---
+++ src/main/java/br/net/silva/daniel/api_brasileirao/usecase/team/domain/UpdateTeamUseCase.java
@@ -0,0 +1,13 @@
+package br.net.silva.daniel.api_brasileirao.usecase.team.domain;
+
+import br.net.silva.daniel.api_brasileirao.domain.team.domain.Team;
+import br.net.silva.daniel.api_brasileirao.usecase.team.interfaces.UseCase;
+
+public class UpdateTeamUseCase implements UseCase<Team> {
+
+
+    @Override
+    public Team execute() {
+        return null;
+    }
+}
