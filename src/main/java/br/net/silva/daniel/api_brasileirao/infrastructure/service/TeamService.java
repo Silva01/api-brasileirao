6307e4eecb1b4ef3208724699a541bb30c05e7ae
silva01 <silva20102@gmail.com>
2023-05-08 14:39:04 -0300

    Implemented method team update

--- src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/service/TeamService.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/service/TeamService.java
@@ -3,6 +3,7 @@ package br.net.silva.daniel.api_brasileirao.infrastructure.service;
 import br.net.silva.daniel.api_brasileirao.domain.shared.repository.FindAllRepository;
 import br.net.silva.daniel.api_brasileirao.domain.shared.repository.FindByIdRepository;
 import br.net.silva.daniel.api_brasileirao.domain.shared.repository.SaveRepository;
+import br.net.silva.daniel.api_brasileirao.domain.shared.repository.UpdateRespository;
 import br.net.silva.daniel.api_brasileirao.domain.team.domain.Team;
 import br.net.silva.daniel.api_brasileirao.domain.team.exception.TeamNotExistsException;
 import br.net.silva.daniel.api_brasileirao.infrastructure.model.TeamModel;
@@ -11,9 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
-public class TeamService implements SaveRepository<Team>, FindAllRepository<Team>, FindByIdRepository<Team> {
+public class TeamService implements SaveRepository<Team>, FindAllRepository<Team>, FindByIdRepository<Team>, UpdateRespository<Team> {
 
     private TeamRepository repository;
 
@@ -40,5 +42,14 @@ public class TeamService implements SaveRepository<Team>, FindAllRepository<Team
     @Override
     public Team findById(Long id) throws TeamNotExistsException {
         return repository.findById(id).map(teamModel -> new Team(teamModel.getName(), teamModel.getLocalidade(), teamModel.getId())).orElseThrow(TeamNotExistsException::new);
+    }
+
+    @Override
+    public Team update(Team aggregate) {
+        TeamModel model = repository.getById(aggregate.getId());
+        model.setName(Objects.nonNull(aggregate.getName()) ? aggregate.getName() : model.getName());
+        model.setLocalidade(Objects.nonNull(aggregate.getLocalidade()) ? aggregate.getLocalidade() : model.getLocalidade());
+        TeamModel response = repository.save(model);
+        return aggregate;
     }
 }
