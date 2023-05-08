b5ce3122295e3983ec7730a1b5ca49c49d646e6a
silva01 <silva20102@gmail.com>
2023-05-08 15:04:42 -0300

    modify exception class

--- src/main/java/br/net/silva/daniel/api_brasileirao/domain/shared/repository/FindByIdRepository.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/domain/shared/repository/FindByIdRepository.java
@@ -1,6 +1,8 @@
 package br.net.silva.daniel.api_brasileirao.domain.shared.repository;
 
 
+import br.net.silva.daniel.api_brasileirao.domain.team.exception.TeamNotExistsException;
+
 public interface FindByIdRepository <T> {
-    T findById(Long id) throws Exception;
+    T findById(Long id) throws TeamNotExistsException;
 }
