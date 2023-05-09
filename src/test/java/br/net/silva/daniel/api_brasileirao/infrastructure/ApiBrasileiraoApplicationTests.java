ad89c3e0c66506d6269710e62c6c242203f699ff
silva01 <silva20102@gmail.com>
2023-05-09 14:57:58 -0300

    added test for team endpoint

--- src/test/java/br/net/silva/daniel/api_brasileirao/infrastructure/ApiBrasileiraoApplicationTests.java
+++ src/test/java/br/net/silva/daniel/api_brasileirao/infrastructure/ApiBrasileiraoApplicationTests.java
@@ -1,4 +1,4 @@
-package br.net.silva.daniel.api_brasileirao.intrastructure;
+package br.net.silva.daniel.api_brasileirao.infrastructure;
 
 import br.net.silva.daniel.api_brasileirao.infrastructure.ApiBrasileiraoApplication;
 import org.junit.jupiter.api.Test;
