47c12fb2814361f900e385e8fc029e909c9feda5
silva01 <silva20102@gmail.com>
2023-05-10 20:08:15 -0300

    created boilerplate for player controller

---
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/controller/PlayerController.java
@@ -0,0 +1,46 @@
+package br.net.silva.daniel.api_brasileirao.infrastructure.controller;
+
+import br.net.silva.daniel.api_brasileirao.domain.player.dto.PlayerDTO;
+import br.net.silva.daniel.api_brasileirao.infrastructure.dto.BodyPlayerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void create(@RequestBody BodyPlayerDTO body) {
+
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void update(@PathVariable("id") Long id, @RequestBody BodyPlayerDTO body) {
+
+
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.OK)
+    public Callable<PlayerDTO> findById(@PathVariable("id") Long id) {
+        return null;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.OK)
+    public Callable<List<PlayerDTO>> findAll() {
+        return null;
+    }
+}
