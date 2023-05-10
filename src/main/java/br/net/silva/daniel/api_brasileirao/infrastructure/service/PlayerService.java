4b3bdd3207a28ea006cfa889ee26809d2d216c68
silva01 <silva20102@gmail.com>
2023-05-10 20:41:09 -0300

    Created service for player

---
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/service/PlayerService.java
@@ -0,0 +1,29 @@
+package br.net.silva.daniel.api_brasileirao.infrastructure.service;
+
+import br.net.silva.daniel.api_brasileirao.domain.player.domain.Player;
+import br.net.silva.daniel.api_brasileirao.domain.shared.repository.SaveRepository;
+import br.net.silva.daniel.api_brasileirao.infrastructure.model.PlayerModel;
+import br.net.silva.daniel.api_brasileirao.infrastructure.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public final class PlayerService implements SaveRepository<Player> {
+
+    private final PlayerRepository playerRepository;
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @Override
+    public Player save(Player aggregate) {
+        PlayerModel playerModel = new PlayerModel();
+        playerModel.setName(aggregate.getName());
+        playerModel.setBirthDate(aggregate.getBirthDate());
+        playerModel.setCountry(aggregate.getCountry());
+        playerModel.setTeam(aggregate.getTeam().getId());
+        playerRepository.save(playerModel);
+        return aggregate;
+    }
+}
