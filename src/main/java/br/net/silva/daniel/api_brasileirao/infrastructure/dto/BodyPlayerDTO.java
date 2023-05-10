7342ebf4d898e302707a2370d42cf0c40132a155
silva01 <silva20102@gmail.com>
2023-05-10 20:07:29 -0300

    created body for Player request

---
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/dto/BodyPlayerDTO.java
@@ -0,0 +1,43 @@
+package br.net.silva.daniel.api_brasileirao.infrastructure.dto;
+
+import java.time.LocalDate;
+
+public class BodyPlayerDTO {
+
+    private String name;
+    private LocalDate birthDate;
+    private String country;
+    private Long teamId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+}
