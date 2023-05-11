1302adf3f0d071f0ef8ee27c466e6ae2b9415c7a
silva01 <silva20102@gmail.com>
2023-05-10 21:14:50 -0300

    added attribute ID

--- src/main/java/br/net/silva/daniel/api_brasileirao/domain/player/domain/Player.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/domain/player/domain/Player.java
@@ -6,16 +6,18 @@ import java.time.LocalDate;
 
 public class Player implements Aggregate<Player> {
 
+    private Long id;
     private final String name;
     private final LocalDate birthDate;
     private final String country;
     private final Long teamId;
 
-    public Player(String name, LocalDate birthDate, String country, Long teamId) {
+    public Player(String name, LocalDate birthDate, String country, Long teamId, Long id) {
         this.name = name;
         this.birthDate = birthDate;
         this.country = country;
         this.teamId = teamId;
+        this.id = id;
     }
 
     public String getName() {
@@ -32,6 +34,10 @@ public class Player implements Aggregate<Player> {
 
     public Long getTeamId() {
         return teamId;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     @Override
