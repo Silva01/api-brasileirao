ef923ee760bcbe193626f4f2a8f58342981894c5
silva01 <silva20102@gmail.com>
2023-05-10 21:16:23 -0300

    Implemented method for find All players

--- src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/service/PlayerService.java
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/service/PlayerService.java
@@ -1,14 +1,17 @@
 package br.net.silva.daniel.api_brasileirao.infrastructure.service;
 
 import br.net.silva.daniel.api_brasileirao.domain.player.domain.Player;
+import br.net.silva.daniel.api_brasileirao.domain.shared.repository.FindAllRepository;
 import br.net.silva.daniel.api_brasileirao.domain.shared.repository.SaveRepository;
 import br.net.silva.daniel.api_brasileirao.infrastructure.model.PlayerModel;
 import br.net.silva.daniel.api_brasileirao.infrastructure.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public final class PlayerService implements SaveRepository<Player> {
+public final class PlayerService implements SaveRepository<Player>, FindAllRepository<Player> {
 
     private final PlayerRepository playerRepository;
     @Autowired
@@ -22,8 +25,17 @@ public final class PlayerService implements SaveRepository<Player> {
         playerModel.setName(aggregate.getName());
         playerModel.setBirthDate(aggregate.getBirthDate());
         playerModel.setCountry(aggregate.getCountry());
-        playerModel.setTeam(aggregate.getTeamId());
+        playerModel.setTeamId(aggregate.getTeamId());
         playerRepository.save(playerModel);
         return aggregate;
+    }
+
+    @Override
+    public List<Player> findAll() {
+        return playerRepository
+                .findAll()
+                .stream()
+                .map(playerModel -> new Player(playerModel.getName(), playerModel.getBirthDate(), playerModel.getCountry(), playerModel.getTeamId(), playerModel.getId()))
+                .toList();
     }
 }
