1115f136002819609f34147168b613a401b644c6
silva01 <silva20102@gmail.com>
2023-05-12 18:12:47 -0300

    Created global handler for error global

---
+++ src/main/java/br/net/silva/daniel/api_brasileirao/infrastructure/exception/GlobalHandler.java
@@ -0,0 +1,18 @@
+package br.net.silva.daniel.api_brasileirao.infrastructure.exception;
+
+import br.net.silva.daniel.api_brasileirao.domain.player.exception.PlayerNotExistsException;
+import br.net.silva.daniel.api_brasileirao.infrastructure.dto.BodyErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalHandler {
+
+    @ExceptionHandler(value = PlayerNotExistsException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Player not found")
+    public BodyErrorDTO playerNotExistsException() {
+        return new BodyErrorDTO(HttpStatus.NOT_FOUND.value(), "Player not found");
+    }
+}
